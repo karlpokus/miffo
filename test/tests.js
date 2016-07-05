1b93f7f94abeab35230473384ceb93e59230a500
Karl Pokus <karl.pokus@gmail.com>
2016-07-05 15:09:20 +0000

    complete test suite

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,10 @@ var test = require('tape'),
     settings = require('./settings.json'),
     url = settings.url,
     collections = settings.collections,
+    user = {
+      username: 'mike',
+      pwd: 'pwd'
+    },
     Miffo = require('../miffo.js'),
     db = new Miffo(url, collections);
     require('console.mute');
@@ -25,15 +29,61 @@ test('constructor', function(t){
   t.end();
 });
 
-test('start', function(t){
+test('.start', function(t){
   console.mute();
   
   db.start(function(){
     var data = console.resume();
     t.equal(data[0], 'db connected', 'connection');
     t.equal(typeof db[collections[0]].find, 'function', 'wrapper API');
-    db.connection.close();
     t.end();
+  });
+});
+
+test('operations', function(t){  
+  db.test.insert(user, function(err, data){
+    t.error(err, 'err null');
+    t.equal(data.result.n, 1, 'insert op');
+    
+    db.test.updateOne({username: user.username}, {$set: {username: 'Julie' }}, function(err, data){
+      t.error(err, 'err null');
+      t.equal(data.result.n, 1, 'update op');
+
+      db.test.deleteOne({username: "Julie"}, function(err, data){
+        t.error(err, 'err null');
+        t.equal(data.result.n, 1, 'delete op');
+        
+        db.test.find({}).toArray(function(err, data){
+          t.error(err, 'err null');
+          t.equal(data.length, 0, 'find op');
+          
+          db.connection.close();
+          t.end();
+        });
+      });
+    });
+  });
+});
+
+test('.oid', function(t){
+  var _id = db.oid(),
+      hex = _id.toHexString();  
+  
+  t.equal(typeof _id, 'object', 'ObjectID');
+  t.equal(hex.length, 24, 'hex');  // check that its 24 chars long
+  t.end();
+});
+
+test('.bcrypt', function(t){
+  db.bcrypt.hash(user.pwd, 10, function(err, hash) {
+    t.error(err, 'err null');    
+    t.equal(typeof hash, 'string', '.hash');
+    
+    db.bcrypt.compare(user.pwd, hash, function(err, res) {
+      t.error(err, 'err null');
+      t.equal(res, true, '.compare');
+      t.end();
+    });
   });
 });
 
